d4594729f92993846d63b3051dcbaa86af0d2b76
kamaluddinprima <muhammad.20035@mhs.unesa.ac.id>
2022-10-04 07:15:34 +0700

    membuat spk

---
+++ application/views/template/navbar.php
@@ -0,0 +1,19 @@
+  <div class="navbar nav-justified navbar-fixed-top navbar-inverse atas">
+    <div class="container">
+      <div class="navbar-header">
+       <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+       </button>
+          <a class="navbar-brand" href="<?=base_url(); ?>" style="color: white">
+            BERANDA
+          </a>
+        </div>
+       <div class="collapse navbar-collapse navHeaderCollapse">
+          <ul class="nav navbar-nav" style="margin-top: 0px; margin-bottom: 0px;">
+            <li class='lina'><a href="<?=base_url('beranda/lihat_semua_motor'); ?>" style="color: white">Daftar Motor</a></li>
+          </ul>
+        </div>    
+       </div>
+  </div>
